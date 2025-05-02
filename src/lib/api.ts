a22091ef38f0d3945bf3697d65a7c6e119d5a91a
Tanimal <bobcheng.bob@gmail.com>
2025-05-02 18:43:50 +0800

    update some api schema

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,20 @@
-import { generateMockHost } from "./mock";
+import * as schema from "./schema";
 
-export function getHostByRack(rack_id: string) {
-  console.log("getHostByRack", rack_id);
-  return generateMockHost();
-}
+type Host = Zod.infer<typeof schema.host_schema>;
+type Rack = Zod.infer<typeof schema.rack_schema>;
+type Room = Zod.infer<typeof schema.room_schema>;
+type Datacenter = Zod.infer<typeof schema.datacenter_schema>;
+type Service = Zod.infer<typeof schema.service_schema>;
 
-export function getRackByRoom(room_id: string) {
-  console.log("getRackByRoom", room_id);
-  return generateMockHost();
-}
+export function addNewHostToRack() {}
+export function getHostsByService() {}
+export function getHostsByRack() {}
+export function modifyHost() {}
+export function deleteHost() {}
+
+export function addNewRackToRoom() {}
+export function getRacksByService() {}
+export function getRacksByRoom() {}
+export function modifyRack() {}
+export function modifyMultipleRacks() {}
+export function deleteRack() {}
