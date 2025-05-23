6596ccf44a342851dcb7b79e3e6d7b72e77e20fa
Tanimal <bobcheng.bob@gmail.com>
2025-05-23 20:59:33 +0800

    move host dialog

--- src/view/host/move-host-dialog.tsx
+++ src/view/host/move-host-dialog.tsx
@@ -181,7 +181,7 @@ export function MoveHostDialog({ items, onSuccess }: MoveItemDialogProps) {
           </DialogDescription>
         </DialogHeader>
 
-        <div className="mb-4">
+        <div>
           {loadingRack ? (
             <div className="flex h-40 items-center justify-center">
               <div className="h-6 w-6 animate-spin rounded-full border-2 border-gray-900 border-t-transparent"></div>
