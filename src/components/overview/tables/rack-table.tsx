9dfbeee1f050956a0171c2aa1d6d1d1d982670b1
C-W-Z <chenweizhang3021@gmail.com>
2025-05-26 13:45:44 +0800

    feat: can not move room

--- src/components/overview/tables/rack-table.tsx
+++ src/components/overview/tables/rack-table.tsx
@@ -91,7 +91,9 @@ export default function RackTable() {
 
         <div className="flex items-center gap-4">
           <RefreshButton isLoading={loading} onClick={() => handleRefresh()} />
-          {user?.role === "admin" && room && <AddRackDialog currentRoom={room} onSuccess={() => handleRefresh()} />}
+          {user?.role === "admin" && room && (
+            <AddRackDialog currentRoom={room} onSuccess={() => handleRefresh()} />
+          )}
         </div>
       </div>
 
