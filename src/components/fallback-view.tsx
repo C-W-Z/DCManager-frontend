049084caa947df4bd81ba85fda5a665831006f95
Tanimal <bobcheng.bob@gmail.com>
2025-05-26 23:34:23 +0800

    update fallback view

--- src/components/fallback-view.tsx
+++ src/components/fallback-view.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function FallbackView({ text, link }: { text: string; link?: string }) {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4">
       <div className="text-lg font-bold">{text}</div>
       {link && (
         <Link to={link} className="ml-2 text-blue-500 hover:underline">
